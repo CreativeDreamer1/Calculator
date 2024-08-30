597de70a0d4cf99198e49f36e62b61ea477f09fe
CreativeDreamer1 <mdawoodnasaryab@gmail.com>
2024-08-30 22:14:06 +0500

    fixed decimal bug

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const buttons = document.querySelectorAll("input[type='button']");
-    const display = document.querySelector('#display');
+	const buttons = document.querySelectorAll("input[type='button']");
+	const display = document.querySelector('#display');
+	let decimalFlag = false;
 
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const num = button.value;
+	buttons.forEach(button => {
 
-            if (num == 'AC') {
-                display.value = "";
-            } else if (num == '=') {
-                display.value = eval(display.value);
-            } else {
-                display.value += num;
-            };
+		button.addEventListener('click', () => {
+			const num = button.value;
 
-        });
-    });
+			if (num == 'AC') {
+				display.value = "";
+				decimalFlag = false;
+
+			} else if (num == '=') {
+				display.value = eval(display.value);
+
+			} else if (num == '.') {
+				if (decimalFlag == false) {
+					display.value += num;
+					decimalFlag = true;
+
+				} else {
+					display.value += "";
+				};
+
+			} else {
+				if (button.className === 'spec') {
+					decimalFlag = false;
+				};
+
+				display.value += num;
+			};
+		});
+	});
 
 });
