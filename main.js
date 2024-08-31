5f9178a463ee2a798c462613bc9511b5df7dbcdd
CreativeDreamer1 <mdawoodnasaryab@gmail.com>
2024-08-31 17:24:20 +0500

    Added '0' as default

--- main.js
+++ main.js
@@ -4,22 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
 	const display = document.querySelector('#display');
 	let decimalFlag = false;
     let completeFlag = false;
+	let operatorFlag = false;
 
 	buttons.forEach(button => {
 
 		button.addEventListener('click', () => {
 			const num = button.value;
 
-			if (num == 'AC') {
-				display.value = "";
+			if (num === 'AC') {
+				display.value = "0";
 				decimalFlag = false;
+				operatorFlag = true;
 
-			} else if (num == '=') {
+			} else if (num === '=') {
 				display.value = eval(display.value);
                 completeFlag = true;
+				operatorFlag = true;
 
-			} else if (num == '.') {
-				if (decimalFlag == false) {
+			} else if (num === '.') {
+				if (decimalFlag === false) {
 					display.value += num;
 					decimalFlag = true;
 
@@ -27,18 +30,31 @@ document.addEventListener('DOMContentLoaded', () => {
 					display.value += "";
 				};
 
-			} else {
-				if (button.className === 'spec') {
-					decimalFlag = false;
-				};
-                if (completeFlag === true) {
-                    display.value = "";
-                    display.value += num;
-                    completeFlag = false;
+			} else if (button.className === 'spec') {
+				decimalFlag = false;
+				if (operatorFlag === true) {
+					display.value += "";
 
-                } else {
+				} else {
+					display.value += num;
+					operatorFlag = false;
+				};
+				operatorFlag = true;
+
+			} else {
+                if (completeFlag === true) {
+                    display.value = num;
+                    completeFlag = false;
+					operatorFlag = false;
+
+                } else if (display.value == 0) {
+					display.value = num;
+				}
+				
+				else {
 				    display.value += num;
-                }
+					operatorFlag = false;
+                };
 			};
 		});
 	});
